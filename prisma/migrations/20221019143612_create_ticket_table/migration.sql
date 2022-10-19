2e0259947efe5156fb406ee72c3dff35c66abd22
Lucas <lugablima@gmail.com>
2022-10-19 11:39:54 -0300

    chore: add migration to create ticket table in database

---
+++ prisma/migrations/20221019143612_create_ticket_table/migration.sql
@@ -0,0 +1,13 @@
+-- CreateTable
+CREATE TABLE "Ticket" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "price" INTEGER NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Ticket_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Ticket_name_key" ON "Ticket"("name");
